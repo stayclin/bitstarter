a8cf147f735fef4bb68583cd05a0ed337a04c499
Stacy Lin <stayclin@gmail.com>
2013-07-15 22:29:41 +0000

    updated port 8080 to web.js

--- web.js
+++ web.js
@@ -4,13 +4,14 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var filename = "index.html";
 
-var data = fs.readFileSync(filename, "utf-8");
+var data = fs.readFileSync(filename).toString();
 
 app.get('/', function(request, response) {
   response.send(data);
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
